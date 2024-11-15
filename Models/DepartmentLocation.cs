bce9d4c24ef9e7094246525f37ca85dc3baf76ba
RicoNguyen49 <Nguyen49@mail.nmc.edu>
2024-11-15 16:04:38 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite Primary Key and foreign key for department 
+        public int DepartmentID { get; set; }
+
+        //Composite Primary key and foreign key for location 
+        public int LocationID { get; set; }
+
+        //Navigation 
+        public Department Department { get; set; } = null!;
+        public Location Location { get; set; } = null!;
+    }
+}
